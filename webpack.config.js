046544a4ba690662b00ec26dd6c76e8c8100d5c9
sakethsomaraju <sakethsomaraju@gmail.com>
2020-09-09 23:53:15 +0530

    setup test database env

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,15 @@
 const path = require('path')
 const ExtarctTextPlugin = require('extract-text-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const webpack = require('webpack')
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+
+if(process.env.NODE_ENV === 'test'){
+    require('dotenv').config({path:'.env.test'})
+}else if(process.env.NODE_ENV === 'development'){
+    require('dotenv').config({path:'.env.development'})
+}
 
 module.exports=(env)=>{
 
@@ -40,7 +49,17 @@ module.exports=(env)=>{
             }]
         },
         plugins:[
-            CSSExtract
+            CSSExtract,
+            new webpack.DefinePlugin({
+                'process.env.FIREBASE_API_KEY':JSON.stringify(process.env.FIREBASE_API_KEY),
+                'process.env.AUTH_DOMAIN':JSON.stringify(process.env.AUTH_DOMAIN),
+                'process.env.DATABASE_URL':JSON.stringify(process.env.DATABASE_URL),
+                'process.env.PROJECT_ID':JSON.stringify(process.env.PROJECT_ID),
+                'process.env.STORAGE_BUCKET':JSON.stringify(process.env.STORAGE_BUCKET),
+                'process.env.MESSAGING_SENDER_ID':JSON.stringify(process.env.MESSAGING_SENDER_ID),
+                'process.env.APP_ID':JSON.stringify(process.env.APP_ID),
+                'process.env.MEASUREMENT_ID':JSON.stringify(process.env.MEASUREMENT_ID),
+            })
         ],
         devtool:isProduction ? 'source-map':'inline-source-map',
         devServer:{
