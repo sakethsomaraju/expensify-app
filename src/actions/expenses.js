72a765d920918a7012f70bf1355e5377a715f116
sakethsomaraju <sakethsomaraju@gmail.com>
2020-09-10 23:16:25 +0530

    wired up edit expense

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -49,6 +49,13 @@ export const editExpense = (id, updates) => ({
   id,
   updates
 });
+export const startEditExpense = (id,updates)=>{
+  return (dispatch)=>{
+    return database.ref(`expenses/${id}`).update(updates).then(()=>{
+      dispatch(editExpense(id,updates))
+    })
+  }
+}
 
 export const setExpenses = (expenses)=>({
   type:'SET_EXPENSES',
