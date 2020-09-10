c5974aee3c2401de8f55c6ca4c5b6ab0be28bbb7
sakethsomaraju <sakethsomaraju@gmail.com>
2020-09-10 21:04:39 +0530

    get fire bases onStart

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ import './styles/styles.scss'
 import AppRouter from './routers/AppRouter'
 import configureStore from './store/configureStore'
 
-import {addExpense} from './actions/expenses'
+import {startSetExpenses} from './actions/expenses'
 import {setTextFilter} from './actions/filters'
 import getVisibleExpenses from './selectors/expenses'
 import 'react-dates/lib/css/_datepicker.css'
@@ -20,4 +20,9 @@ const jsx = (
     </Provider>
 )
 
-ReactDOM.render(jsx,document.getElementById('app'))
+ReactDOM.render(<p>loading...</p>,document.getElementById('app'))
+
+store.dispatch(startSetExpenses()).then(()=>{
+    ReactDOM.render(jsx,document.getElementById('app'))
+})
+
--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,10 +1,18 @@
-import { startAddExpense,addExpense ,editExpense, removeExpense } from '../../actions/expenses';
+import { startAddExpense,addExpense ,editExpense, removeExpense,setExpenses } from '../../actions/expenses';
 import expenses from '../fixtures/expenses'
 import configureMockStore from 'redux-mock-store'
 import thunk from 'redux-thunk'
 import database from '../../firebase/firebase'
 
 const createMockStore = configureMockStore([thunk])
+
+beforeEach((done)=>{
+  const expensesData = {}
+  expenses.forEach(({id,description,amount,createdAt,note})=>{
+    expensesData[id]={description,amount,createdAt,note}
+  })
+  database.ref('expenses').set(expensesData).then(()=>done())
+})
 
 test('should setup remove expense action object', () => {
   const action = removeExpense({ id: '123abc' });
@@ -93,3 +101,12 @@ test('should add expense to database and store with defaults',(then)=>{
 //     }
 //   });
 // });
+
+test('Should setup setExpenses action',()=>{
+  const action = setExpenses(expenses)
+  expect(action).toEqual({
+    type:'SET_EXPENSES',
+    expenses
+  })
+})
+
